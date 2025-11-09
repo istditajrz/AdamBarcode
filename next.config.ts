a3a71a05bbaffd7b1fc2eb16464baa56f861041c
istdita <istditajrz@gmail.com>
2025-11-09 19:04:41 +0000

    Containerize

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,6 @@
 import { type NextConfig } from "next";
-// import { ZBAR_WASM_REPOSITORY } from '@undecaf/barcode-detector-polyfill';
 
 const conf: NextConfig = {
-    // turbopack: {
-    //     rules: {
-    //         '*': {
-    //             loaders: [{
-    //                 loader: 'string-replace-loader',
-    //                 options: {
-    //                     search: ZBAR_WASM_REPOSITORY,
-    //                     replace: '@undecaf/zbar-wasm',
-    //                     flags: 'g'
-    //                 }
-    //             }],
-    //         }
-    //     }
-    // }
+	output: 'standalone'
 };
 export default conf;
